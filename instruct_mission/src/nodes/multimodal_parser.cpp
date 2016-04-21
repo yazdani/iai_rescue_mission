650d2aaf99c24268850ec0242ecd58670848fc92
Fereshta Yazdani <yazdani@cs.uni-bremen.de>
2016-04-21 19:47:17 +0200

    added new commands

--- instruct_mission/src/nodes/multimodal_parser.cpp
+++ instruct_mission/src/nodes/multimodal_parser.cpp
@@ -39,6 +39,7 @@
 #include <sstream>
 #include <sys/types.h>
 #include <unistd.h>
+#include <boost/algorithm/string.hpp>
 
 int sockfd, newsockfd, portno, clilen;
 
@@ -46,6 +47,107 @@ struct sockaddr_in serv_addr, cli_addr;
 
 
 using namespace std;
+
+vector<string> splitString(string input, string delimiter)
+{
+  vector<string> output;
+  
+  split(output, input, is_any_of(delimiter), token_compress_on);
+  
+  return output;
+}
+
+string without_brakets(string value)
+{
+  
+  std::vector<string> no_brackets = splitString(value, ";");
+  std::vector<string> new_string;
+  string s2 = "<=inside";
+  std::string token;
+  std::string token1;
+  std::vector<string> cmds;
+  std::vector<string> vec;
+  
+  
+  //move(right,tree)<=inside(left,house) --> move(right,tree);move(left,house)
+  for(int i = 0; i < no_brackets.size(); i++)
+    {
+      if(strstr(no_brackets[i].c_str(),s2.c_str())) //inside
+	{
+	  token = no_brackets[i].substr(0, no_brackets[i].find("(")); //gives first action
+	  token = ";"+token;
+	  boost::replace_all(no_brackets[i],"<=inside",token);
+	}    
+      
+      new_string.push_back(no_brackets[i]);
+    }
+  
+  //move(right,house);move(left,tree) --> split into two vectors
+  int pointers = 0;
+  for(int j = 0; j < new_string.size(); j++)
+    {
+      std::vector<string> test = splitString(new_string[j], ";");
+      
+      if(test.size() > 1)
+	{
+	  while(pointers < test.size())
+	    {
+	      cmds.push_back(test[pointers]);
+	      pointers++;
+	    }
+	  pointers = 0;
+	}else
+	{
+	  cmds.push_back(new_string[j]);
+	}
+      
+    }
+  
+  std::vector<string> new_vec;
+  for(int index = 0; index < cmds.size(); index++)
+    {
+      boost::replace_all(cmds[index],"pointed_at(","pointed_at,");
+      boost::replace_all(cmds[index],"broken(","broken,");
+      boost::replace_all(cmds[index],"big(","big,");
+      boost::replace_all(cmds[index],"small(","small,");
+      boost::replace_all(cmds[index],"first(","first,");
+      boost::replace_all(cmds[index],"second(","second,");
+      boost::replace_all(cmds[index],"third(","third,");
+      boost::replace_all(cmds[index],"fourth(","fourth,");
+      boost::replace_all(cmds[index],"last(","last,");
+      boost::replace_all(cmds[index],"))",")");
+    }
+  
+  for(int sup = 0; sup < cmds.size(); sup++)
+    {
+      std::vector<string> testing = splitString(cmds[sup], ",");
+      if(testing.size() == 1)
+	{
+	  boost::replace_all(cmds[sup],"picture)","nil,nil,picture)");
+	  boost::replace_all(cmds[sup],"right)","right,nil,nil)");
+	  boost::replace_all(cmds[sup],"left)","left,nil,nil)");
+	  boost::replace_all(cmds[sup],"ahead)","ahead,nil,nil)");
+	}else 
+	if(testing.size() == 2)
+	  {
+	    boost::replace_all(cmds[sup],",",",nil,");
+	  }
+    }
+  string toktok = "";
+  
+  for(int pointer = 0; pointer < cmds.size(); pointer++)
+    {
+      if(pointer != cmds.size()-1)
+	{
+	  toktok = toktok + cmds[pointer]+ ";";
+	}else
+	{
+	  toktok = toktok + cmds[pointer];
+	}
+    }
+  return toktok;
+}
+
 
 string interpretCommand(string cmd)
 {
@@ -104,7 +206,7 @@ bool parser(instruct_mission::multimodal_parser::Request  &req,
   cmd_interpreted =  interpretCommand(req.goal);
   ROS_INFO_STREAM("cmd_interpreted");
   ROS_INFO_STREAM(cmd_interpreted);
-  res.result = cmd_interpreted;
+  res.result = without_brakets(cmd_interpreted);
   return true;
 }
 
